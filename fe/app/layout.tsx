e4e878382e52a8a1e214b28e9ebe68590641a8bd
hua-lun <chuahualun@gmail.com>
2025-09-21 00:45:15 +0800

    Edit footer padding

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <footer className="w-full flex items-center justify-center pb-1">
               <Link
                 isExternal
                 aria-label="Github"
