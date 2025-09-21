8db02c47b142bf5edf690d8d5ec555f5c47ccc67
hua-lun <chuahualun@gmail.com>
2025-09-21 16:42:15 +0800

    Add new font size for home screen

--- fe/components/primitives.ts
+++ fe/components/primitives.ts
@@ -16,6 +16,7 @@ export const title = tv({
       sm: "text-3xl lg:text-4xl",
       md: "text-[2.3rem] lg:text-5xl",
       lg: "text-4xl lg:text-6xl",
+      home: "text-xl sm:text-5xl",
     },
     fullWidth: {
       true: "w-full block",
